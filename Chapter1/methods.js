4ee44a98895481ec3b2ace82b13cab483f267312
Youssef Eltonsy <yousseftonsy65@gmail.com>
2026-01-04 15:39:04 +0200

    Initial commit

---
+++ Chapter1/methods.js
@@ -0,0 +1,8 @@
+let email = 'youssef@gmail.com';
+
+// let result = email.lastIndexOf('m')
+// let result = email.slice(0,7)
+// let result = email.substr(4,10)
+let result = email.replace('m', 'w')    //first letter that it finds only
+
+console.log(result)
---
+++ Chapter1/sandbox.js
@@ -0,0 +1,33 @@
+let age = 23;
+let year = 2026;
+
+console.log(age, year);
+
+age = 24;
+console.log(age)
+
+
+const points = 100;
+console.log(points);
+//var cannot change value
+
+var score = 75;
+console.log(score);
+//older way to use var
+
+let name = 'youssef';
+console.log(name);
+
+//get character from string
+console.log(name[0]);
+
+//string length
+console.log(name.length)
+
+//string methods
+console.log(name.toUpperCase())
+let result = name.toUpperCase()
+console.log(result, name);
+
+let index = name.indexOf('s');
+console.log(index)
