2afce587756a36fc61175f85db06f8784ba16a1e
Youssef Eltonsy <yousseftonsy65@gmail.com>
2026-01-04 16:27:17 +0200

    added arrays

---
+++ Chapter1/arrays.js
@@ -0,0 +1,22 @@
+let names = ['Ahmed', 'Youssef', "Ali"];
+// names[2]= 'yehia'
+
+// console.log(names[2]);
+
+// let ages = [20, 24, 25]
+// console.log(ages);
+
+// let random = ['youssef', 24];
+// console.log(random);
+
+// console.log(names.length)
+
+// let result = names.join('-');
+// let result = names.indexOf('Youssef')
+// let result = names.concat(['joe'])
+let result = names.push('joe');
+result = names.pop();
+
+result = names.includes('joseph')
+
+console.log(result);
