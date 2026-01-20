69ac63350975b76de79311cea54d9f2eb31995b6
Youssef Eltonsy <yousseftonsy65@gmail.com>
2026-01-20 19:47:17 +0200

    added json todos

--- Chapter6/HTTP.js
+++ Chapter6/HTTP.js
@@ -1,11 +1,34 @@
-const request = new XMLHttpRequest();
+const getTodos = (resource, callBack) => {
+    const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
     // console.log(request, request.readyState);
-    if (request.readyState === 4) {
-        console.log(request, request.responseText);
+    if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText)
+        callBack(undefined,data);
+    }
+    else if (request.readyState === 4) {
+        callBack('could not fetch data', undefined);
     }
 })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todosj/')
+request.open('GET', resource)
 request.send();
+}
+
+
+console.log(1);
+console.log(2);
+
+//async code
+getTodos('todos/youssef.json', (err, data) => {
+    console.log("callBack fired")
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+    }
+})
+
+console.log(3);
+console.log(4);
